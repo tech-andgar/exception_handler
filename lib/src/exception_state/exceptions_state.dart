a1b0ad9d16bff20e73eba30480857ac18c08a113
Andres Garcia <andgar2010@gmail.com>
2024-02-12 21:36:40 -0500

    chore(exception_handler): Add license headers to source files
    Add Apache-2.0 license headers to all source files in the exception_handler module. This ensures compliance with licensing requirements and provides clear information about copyright and usage rights.

--- lib/src/exception_state/exceptions_state.dart
+++ lib/src/exception_state/exceptions_state.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 import 'dart:developer';
 
 import '../utils/utils.dart';
--- lib/src/result_state/result_state.dart
+++ lib/src/result_state/result_state.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 import '../exception_state/exceptions_state.dart';
 import '../utils/utils.dart';
 
