b79e717548f9ce6def7a531977c150dafcc21e9b
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-07 21:12:31 -0500

    feat(exception_handler): Add copyright notice
    Added Apache-2.0 license copyright notices to the top of several files in the exception handler module.

--- lib/src/exception_handler/dio/dio.dart
+++ lib/src/exception_handler/dio/dio.dart
@@ -1,2 +1,6 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 export 'dio_http_response_extension.dart';
 export 'exception_handler_dio.dart';
--- lib/src/src.dart
+++ lib/src/src.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 export 'exception_handler/exception_handler_client.dart';
 export 'exception_state/exceptions_state.dart';
 export 'result_state/result_state.dart';
