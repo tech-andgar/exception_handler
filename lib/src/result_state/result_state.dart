e1a364d86fdd12251c0b8a97e56ee3fbfcf3ee21
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-07 20:57:40 -0500

    docs(result_state): Update class definitions
    Updated the ResultState base class and its subclasses, SuccessState and FailureState. The classes are now final and include generic type [TModel] in their documentation for better clarity.

--- lib/src/result_state/result_state.dart
+++ lib/src/result_state/result_state.dart
@@ -5,13 +5,14 @@
 import '../exception_state/exceptions_state.dart';
 import '../utils/utils.dart';
 
-/// An sealed base class to represent the different states of result.
+/// An sealed base ResultState class
+/// to [TModel] represents the different states of result.
 sealed class ResultState<TModel> extends CustomEquatable {
   const ResultState();
 }
 
-/// Success status with a generic value T.
-class SuccessState<TModel> extends ResultState<TModel> {
+/// Success status with a generic value [TModel].
+final class SuccessState<TModel> extends ResultState<TModel> {
   const SuccessState(this.data);
 
   final TModel data;
@@ -21,7 +22,7 @@ class SuccessState<TModel> extends ResultState<TModel> {
 }
 
 /// Error status with a specific type of exception.
-class FailureState<TModel> extends ResultState<TModel> {
+final class FailureState<TModel> extends ResultState<TModel> {
   const FailureState(this.exception);
 
   final ExceptionState<TModel> exception;
