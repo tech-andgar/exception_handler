f380256b7372bc48ecf9279c65555a7af2f0e179
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-09 18:11:28 -0500

    refactor(exception_state): Simplify exception handling
    Simplified the exception handling by removing specific enums for different types of exceptions and replacing them with string messages. This makes the code more maintainable and easier to understand. Also, added a new class for handling unknown exceptions.

--- lib/src/exception_state/exceptions_state.dart
+++ lib/src/exception_state/exceptions_state.dart
@@ -1,259 +1,188 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 import 'dart:developer';
 
+import 'package:http_exception/http_exception.dart';
+
 import '../utils/utils.dart';
-
-// Enums defining different types of exceptions for clear and specific error
-// handling.
-
-enum NetworkException {
-  noInternetConnection,
-  timeOutException,
-  unknown,
-}
-
-enum CacheException {
-  unknown,
-}
-
-enum InvalidInputException {
-  unknown,
-}
-
-enum HttpException {
-  // 1xx - Informative Responses
-  continue_,
-  switchingProtocols,
-  processing,
-  earlyHints,
-  // 2xx - Successful Responses
-  oK,
-  created,
-  accepted,
-  nonAuthoritativeInformation,
-  noContent,
-  resetContent,
-  partialContent,
-  multiStatus,
-  alreadyReported,
-  iMUsed,
-  // 3xx - Redirections
-  movedPermanently,
-  found,
-  notModified,
-  useProxy,
-  switchProxy,
-  temporaryRedirect,
-  unknownRedirect,
-  // 4xx - Client Errors
-  badRequest,
-  unauthorized,
-  forbidden,
-  notFound,
-  methodNotAllowed,
-  notAcceptable,
-  proxyAuthenticationRequired,
-  requestTimeout,
-  conflict,
-  gone,
-  lengthRequired,
-  preconditionFailed,
-  payloadTooLarge,
-  uRITooLong,
-  unsupportedMediaType,
-  rangeNotSatisfiable,
-  expectationFailed,
-  imATeapot,
-  misdirectedRequest,
-  unprocessableEntity,
-  locked,
-  failedDependency,
-  upgradeRequired,
-  preconditionRequired,
-  tooManyRequests,
-  requestHeaderFieldsTooLarge,
-  unavailableForLegalReasons,
-  unknownClient,
-  // 5xx - Server Errors
-  internalServerError,
-  notImplemented,
-  serviceUnavailable,
-  gatewayTimeout,
-  badGateway,
-  hTTPVersionNotSupported,
-  variantAlsoNegotiates,
-  insufficientStorage,
-  loopDetected,
-  notExtended,
-  networkAuthenticationRequired,
-  bandwidthLimitExceeded,
-  unknownError,
-  webServerIsDown,
-  connectionTimedOut,
-  originIsUnreachable,
-  aTimeoutOccurred,
-  sSLHandshakeFailed,
-  invalidSSLCertificate,
-  unknownServer,
-  unknown,
-}
 
 /// [ExceptionState] sealed class represents a generic state for handling
 /// various exceptions.
 ///
 /// It includes optional fields for different exception types.
-sealed class ExceptionState<T> extends CustomEquatable {
-  final CacheException? cacheException;
-  final Exception? clientException;
-  final InvalidInputException? invalidInputException;
-  final Exception? parseException;
-  final HttpException? httpException;
-  final NetworkException? networkException;
-  final StackTrace stackTrace;
-
+sealed class ExceptionState<TModel> extends CustomEquatable
+    implements Exception {
   const ExceptionState({
-    this.cacheException,
-    this.clientException,
-    this.invalidInputException,
-    this.parseException,
-    this.httpException,
-    this.networkException,
+    required this.message,
     required this.stackTrace,
   });
+
+  /// A message describing the format error.
+  final String message;
+  final StackTrace stackTrace;
 }
 
 /// [DataClientExceptionState] captures exceptions related to client-side
 /// issues.
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataClientExceptionState<T> extends ExceptionState<T> {
-  DataClientExceptionState(Exception exception, StackTrace stackTrace)
-      : super(clientException: exception, stackTrace: stackTrace) {
+class DataClientExceptionState<TModel> extends ExceptionState<TModel> {
+  DataClientExceptionState({
+    required String message,
+    required StackTrace stackTrace,
+  }) : super(message: message, stackTrace: stackTrace) {
     log(
       'Client exception captured:',
-      error: exception,
+      error: message,
       stackTrace: stackTrace,
       name: 'DataClientExceptionState',
     );
   }
 
   @override
-  Map<String, Object?> get namedProps => {'clientException': clientException};
+  Map<String, Object?> get namedProps => {'clientException': message};
 }
 
 /// [DataParseExceptionState] handles exceptions related to parsing issues
 /// (e.g., JSON parsing).
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataParseExceptionState<T> extends ExceptionState<T> {
-  DataParseExceptionState(Exception exception, StackTrace stackTrace)
-      : super(parseException: exception, stackTrace: stackTrace) {
+class DataParseExceptionState<TModel> extends ExceptionState<TModel> {
+  DataParseExceptionState({
+    required StackTrace stackTrace,
+    String message = 'Error parsing data.',
+  }) : super(message: message, stackTrace: stackTrace) {
     log(
       'Unable to parse the json:',
-      error: exception,
+      error: message,
       stackTrace: stackTrace,
       name: 'DataParseExceptionState',
     );
   }
 
   @override
-  Map<String, Object?> get namedProps => {'parseException': parseException};
+  Map<String, Object?> get namedProps => {'parseException': message};
 }
 
 /// [DataHttpExceptionState] is used for handling HTTP-related exceptions.
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataHttpExceptionState<T> extends ExceptionState<T> {
+class DataHttpExceptionState<TModel> extends ExceptionState<TModel> {
   DataHttpExceptionState({
-    required HttpException httpException,
+    required this.httpException,
     required StackTrace stackTrace,
     Exception? exception,
-    this.statusCode,
-  }) : super(httpException: httpException, stackTrace: stackTrace) {
+  }) : super(message: httpException.toString(), stackTrace: stackTrace) {
     log(
-      'A $httpException${statusCode != null ? ' $statusCode' : null} captured:',
-      error: exception,
+      'A $httpException, captured:',
+      error: exception.toString(),
       stackTrace: stackTrace,
       name: 'DataHttpExceptionState',
     );
   }
 
-  final int? statusCode;
+  final HttpException httpException;
 
   @override
-  Map<String, Object?> get namedProps => {
-        'httpException': httpException,
-        'statusCode': statusCode,
-      };
+  Map<String, Object?> get namedProps => {'httpException': httpException};
 }
 
 /// [DataNetworkExceptionState] is for handling network-related exceptions
 /// (e.g., no internet connection).
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataNetworkExceptionState<T> extends ExceptionState<T> {
-  DataNetworkExceptionState(NetworkException exception, StackTrace stackTrace)
-      : super(networkException: exception, stackTrace: stackTrace) {
+class DataNetworkExceptionState<TModel> extends ExceptionState<TModel> {
+  DataNetworkExceptionState({
+    required StackTrace stackTrace,
+    String message = 'A network error occurred.',
+  }) : super(message: message, stackTrace: stackTrace) {
     log(
       'Network exception captured:',
-      error: exception,
+      error: message,
       stackTrace: stackTrace,
       name: 'DataNetworkExceptionState',
     );
   }
 
   @override
-  Map<String, Object?> get namedProps => {'networkException': networkException};
+  Map<String, Object?> get namedProps => {'networkException': message};
 }
 
 /// [DataCacheExceptionState] is used for handling exceptions related to data
-/// caching.
+/// caching operations.
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataCacheExceptionState<T> extends ExceptionState<T> {
-  DataCacheExceptionState(CacheException exception, StackTrace stackTrace)
-      : super(cacheException: exception, stackTrace: stackTrace) {
+class DataCacheExceptionState<TModel> extends ExceptionState<TModel> {
+  DataCacheExceptionState({
+    required StackTrace stackTrace,
+    String message = 'A cache error occurred.',
+  }) : super(message: message, stackTrace: stackTrace) {
     log(
       'Cache exception captured:',
-      error: exception,
+      error: message,
       stackTrace: stackTrace,
       name: 'DataCacheExceptionState',
     );
   }
 
   @override
-  Map<String, Object?> get namedProps => {'cacheException': cacheException};
+  Map<String, Object?> get namedProps => {'cacheException': message};
 }
 
 /// [DataInvalidInputExceptionState] is used for handling exceptions related to
 /// invalid input data.
 ///
-/// This exception class extends [ExceptionState], providing a generic type [T]
-/// to allow encapsulating additional information or data related to
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
 /// the exception.
-class DataInvalidInputExceptionState<T> extends ExceptionState<T> {
-  DataInvalidInputExceptionState(
-    InvalidInputException exception,
-    StackTrace stackTrace,
-  ) : super(invalidInputException: exception, stackTrace: stackTrace) {
+class DataInvalidInputExceptionState<TModel> extends ExceptionState<TModel> {
+  DataInvalidInputExceptionState({
+    required StackTrace stackTrace,
+    String message = 'Invalid input provided.',
+  }) : super(message: message, stackTrace: stackTrace) {
     log(
       'Invalid Input exception captured:',
-      error: exception,
+      error: message,
       stackTrace: stackTrace,
       name: 'DataInvalidInputExceptionState',
     );
   }
 
   @override
-  Map<String, Object?> get namedProps =>
-      {'invalidInputException': invalidInputException};
+  Map<String, Object?> get namedProps => {'invalidInputException': message};
+}
+
+/// [DataUnknownExceptionState] is used for handling exceptions related to
+/// Unknown error data.
+///
+/// This exception class extends [ExceptionState], providing a generic type
+/// [TModel] to allow encapsulating additional information or data related to
+/// the exception.
+class DataUnknownExceptionState<TModel> extends ExceptionState<TModel> {
+  DataUnknownExceptionState({
+    required String message,
+    required StackTrace stackTrace,
+  }) : super(message: message, stackTrace: stackTrace) {
+    log(
+      'Unknown exception captured:',
+      error: message,
+      stackTrace: stackTrace,
+      name: 'DataUnknownExceptionState',
+    );
+  }
+
+  @override
+  Map<String, Object?> get namedProps => {'unknownException': message};
 }
