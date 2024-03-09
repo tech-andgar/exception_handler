5f9c65aa7dac9323db36d5ff263f0b4f63de062b
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-09 18:15:03 -0500

    feat(exception_handler): Add Dio exception handling
    Introduced a new file for handling exceptions with the Dio library. This includes methods to handle HTTP responses, status codes, and connectivity issues. Also refactored the ClientExceptionHandler from an abstract class to a mixin and updated its method signatures for better flexibility. Removed old exception handler implementation.

--- lib/src/exception_handler/exception_handler_client.dart
+++ lib/src/exception_handler/exception_handler_client.dart
@@ -1,10 +1,18 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 import '../../exception_handler.dart';
 
-export 'exception_handler_dio.dart';
+export 'dio/dio.dart';
 export 'typedef.dart';
 
-abstract class ClientExceptionHandler {
-  /// callApi is a generic method to handle API calls and return a tuple of
+mixin ClientExceptionHandler {
+  /// Method [callApi] is a generic method to handle API calls and return a tuple of
   /// ExceptionState and parsed data.
-  Future<ResultState<T>> callApi<R, T>(ApiHandler<R, T> apiHandler);
+  ///
+  Future<ResultState<TModel>> callApi<R, TModel>(
+    ApiHandler<R, TModel> apiHandler, {
+    HandleHttpParseResponse<R, TModel>? handleHttpParseResponse,
+  });
 }
