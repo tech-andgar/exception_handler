0620fbd819f5fba39358cebd2fce08768c3840cc
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-09 18:10:59 -0500

    refactor(result_state): Update class and variable names
    Updated the naming convention for classes and variables in result_state.dart. Replaced generic type 'T' with 'TModel' for better readability. Also, added copyright notice at the top of the file.

--- lib/src/result_state/result_state.dart
+++ lib/src/result_state/result_state.dart
@@ -1,26 +1,31 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 import '../exception_state/exceptions_state.dart';
 import '../utils/utils.dart';
 
-/// An sealed base class to represent the different states of result.
-sealed class ResultState<T> extends CustomEquatable {
+/// An sealed base ResultState class
+/// to [TModel] represents the different states of result.
+sealed class ResultState<TModel> extends CustomEquatable {
   const ResultState();
 }
 
-/// Success status with a generic value T.
-class SuccessState<T> extends ResultState<T> {
+/// Success status with a generic value [TModel].
+final class SuccessState<TModel> extends ResultState<TModel> {
   const SuccessState(this.data);
 
-  final T data;
+  final TModel data;
 
   @override
   Map<String, Object?> get namedProps => {'data': data};
 }
 
 /// Error status with a specific type of exception.
-class FailureState<T> extends ResultState<T> {
+final class FailureState<TModel> extends ResultState<TModel> {
   const FailureState(this.exception);
 
-  final ExceptionState<T> exception;
+  final ExceptionState<TModel> exception;
 
   @override
   Map<String, Object?> get namedProps => {'exception': exception};
