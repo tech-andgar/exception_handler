d65e74a500f31f4c5d546a234fda2a07fc933153
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-03-09 18:09:49 -0500

    feat(utils): Add isolates utility for async computation
    Introduced a new utility for handling asynchronous computations using isolates. This includes separate implementations for web and IO environments, along with constants to determine the application's compile mode. The compute function in this utility can be used to run expensive operations in the background without blocking the UI. Also updated utils.dart to export this new functionality.

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,2 +1,6 @@
+// Copyright (c) 2024, TECH-ANDGAR.
+// All rights reserved. Use of this source code
+// is governed by a Apache-2.0 license that can be found in the LICENSE file.
+
 export 'custom_equatable.dart';
-export 'extensions.dart';
+export 'isolates/isolates.dart';
