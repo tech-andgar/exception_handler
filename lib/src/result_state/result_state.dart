ce4a467f8ee2d6c483e29b933c77e8fb127c2ee3
Andrés García <dev@tech-andgar.me>
2025-01-16 14:10:42 -0500

    feat: add new class Result/Ok/Error are inspired by official Dart lang Doc

--- lib/src/result_state/result_state.dart
+++ lib/src/result_state/result_state.dart
@@ -7,11 +7,13 @@ import '../utils/utils.dart';
 
 /// An sealed base ResultState class
 /// to [TModel] represents the different states of result.
+@Deprecated('Use class Result() instead.')
 sealed class ResultState<TModel> extends CustomEquatable {
   const ResultState();
 }
 
 /// Success status with a generic value [TModel].
+@Deprecated('Use class OK() instead.')
 final class SuccessState<TModel> extends ResultState<TModel> {
   const SuccessState(this.data);
 
@@ -22,6 +24,7 @@ final class SuccessState<TModel> extends ResultState<TModel> {
 }
 
 /// Error status with a specific type of exception.
+@Deprecated('Use class Error() instead.')
 final class FailureState<TModel> extends ResultState<TModel> {
   const FailureState(this.exception);
 
@@ -29,4 +32,46 @@ final class FailureState<TModel> extends ResultState<TModel> {
 
   @override
   Map<String, Object?> get namedProps => {'exception': exception};
+}
+
+/// Utility class that simplifies handling errors.
+/// Inspired by the [Result] class from the official Dart language documentation https://docs.flutter.dev/app-architecture/design-patterns/result.
+///
+/// Return a [Result] from a function to indicate success or failure.
+///
+/// A [Result] is either an [Ok] with a value of type [T]
+/// or an [Error] with an [Exception].
+///
+/// Use [Result.ok] to create a successful result with a value of type [T].
+/// Use [Result.error] to create an error result with an [Exception].
+sealed class Result<T> extends CustomEquatable {
+  const Result();
+
+  /// Creates an instance of Result containing a value.
+  factory Result.ok(final T value) => Ok(value);
+
+  /// Create an instance of Result containing an error.
+  factory Result.error(final ExceptionState<T> error) => Error(error);
+}
+
+/// Subclass of Result for values.
+final class Ok<T> extends Result<T> {
+  const Ok(this.value);
+
+  /// Returned value in result.
+  final T value;
+
+  @override
+  Map<String, Object?> get namedProps => {'value': value};
+}
+
+/// Subclass of Result for errors.
+final class Error<T> extends Result<T> {
+  const Error(this.error);
+
+  /// Returned error in result.
+  final ExceptionState<T> error;
+
+  @override
+  Map<String, Object?> get namedProps => {'error': error};
 }
